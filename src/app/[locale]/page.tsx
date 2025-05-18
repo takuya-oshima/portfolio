96166344ad1cca1d8064f755c4a194e5763cfca8
takuya-oshima <o.takuya.zero@gmail.com>
2025-05-18 11:47:12 +0900

    星屑objectをコンポーネントファイル化

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,39 +1,11 @@
 "use client";
 import {Link} from "@/i18n/routing";
-import { useEffect } from "react";
+import BackgroundStar from "@/components/BackgroundStar";
 
 export default function HomePage() {
-  useEffect(() => {
-    const stars = document.querySelector(".stars");
-
-    if (!stars) return;
-
-    const createStar = () => {
-      const starEl = document.createElement("span");
-      starEl.className = "star";
-      const minSize = 1;
-      const maxSize = 2;
-      const size = Math.random() * (maxSize - minSize) + minSize;
-      starEl.style.width = `${size}px`;
-      starEl.style.height = `${size}px`;
-      starEl.style.left = `${Math.random() * 100}%`;
-      starEl.style.top = `${Math.random() * 100}%`;
-      starEl.style.position = "absolute";
-      starEl.style.background = "white";
-      starEl.style.borderRadius = "50%";
-      starEl.style.animation = "twinkle 1.5s infinite";
-      starEl.style.animationDelay = `${Math.random() * 10}s`;
-      stars.appendChild(starEl);
-    };
-
-    for (let i = 0; i <= 50; i++) {
-      createStar();
-    }
-  }, []);
-
   return (
     <div className="h-fit">
-      <div className="stars"></div>
+      <BackgroundStar />
       <div className="mt-[18vh] md:mt-24 lg:mt-8 mb-[15vh] lg:mb-32 text-center tracking-wide">
         <h1 className="mb-8 md:mb-14 text-5xl md:text-8xl lg:text-index-title font-normal whitespace-nowrap">Takuya Oshima</h1>
         <h2 className="text-lg md:text-xl lg:text-3.5xl font-normal">Web Designer / Frontend Developer</h2>
---
+++ src/components/BackgroundStar/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useEffect } from "react";
+
+export default function BackgroundStar() {
+  useEffect(() => {
+    //背景に星屑のobjectを生成
+    const stars = document.querySelector(".stars");
+
+    if (!stars) return;
+
+    const createStar = () => {
+      const starEl = document.createElement("span");
+      starEl.className = "star";
+      const minSize = 1;
+      const maxSize = 2;
+      const size = Math.random() * (maxSize - minSize) + minSize;
+      starEl.style.width = `${size}px`;
+      starEl.style.height = `${size}px`;
+      starEl.style.left = `${Math.random() * 100}%`;
+      starEl.style.top = `${Math.random() * 100}%`;
+      starEl.style.position = "absolute";
+      starEl.style.background = "white";
+      starEl.style.borderRadius = "50%";
+      starEl.style.animation = "twinkle 1.5s infinite";
+      starEl.style.animationDelay = `${Math.random() * 10}s`;
+      stars.appendChild(starEl);
+    };
+    for (let i = 0; i <= 50; i++) {
+      createStar();
+    }
+  }, []);
+
+  return(
+    <div className="stars"></div>
+  );
+};
