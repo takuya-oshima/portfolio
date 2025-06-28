32c35f9e23e4f9b1285f6c45cf4011e6f0f7d1a3
takuya-oshima <o.takuya.zero@gmail.com>
2025-06-28 12:16:33 +0900

    微調整

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -120,7 +120,7 @@ export default async function LocaleLayout({
             </NextIntlClientProvider>
           </LenisWrapper>
         </ThemeWrapper>
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
--- src/components/ProfileContent/index.tsx
+++ src/components/ProfileContent/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+//import Image from "next/image";
 import PageTitleSide from "@/components/PageTitleSide";
 import PageTitleHead from "@/components/PageTitleHead";
 import PageLead from "@/components/PageLead";
