3b0483a659123b95d1493385f73b84424a333d08
takuya-oshima <o.takuya.zero@gmail.com>
2025-07-25 13:16:57 +0900

    next.configファイルの設定を見直し

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,22 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
   trailingSlash: true, //URLの末尾にスラッシュを付与
-  reactStrictMode: true, // Reactの厳密モードを有効化
+  reactStrictMode: true, //Reactの厳密モードを有効化
   //MicroCMSの画像の制限設定
   images: {
+    formats: ['image/webp'], //WebP優先
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.microcms-assets.io",
       },
     ],
+  },
+  experimental: {
+    optimizeCss: true, //CSS最適化（Vercel環境で有効）
+  },
+  compiler: {
+    styledComponents: true, // styled-components 使用時のみ
   },
 };
 export default withNextIntl(nextConfig);
